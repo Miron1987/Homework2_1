3e195f221b81bf6a796c6ea1727b8a26862bdc0d
Aleksandr Kougiya <zero0072002@mail.ru>
2017-11-05 13:54:41 +0300

    HM_2

---
+++ Homework2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = 0;
+            decimal money = 0;
+
+            List<Worker> workers = Worker.GetSampleWorkers();
+            List<Manager> managers = Manager.GetSampleManagers();
+
+
+            var _workers = from worker in workers
+                           select worker.Payment;
+
+            var _managers = from manager in managers
+                            select manager.Payment;
+
+
+            foreach (var wor in _workers)
+            {
+                people += 1;
+                money += wor;
+            }
+
+            foreach (var manag in _managers)
+            {
+                people += 1;
+                money += manag;
+            }
+
+            Console.WriteLine($"Работников: {people}, Средняя зарплата: {Decimal.Round(money / people, 2)} руб.");
+
+            var best_wor = from _worker in workers
+                           where _worker.Useful == BestOfWorkers(workers)
+                           select _worker;
+
+            foreach (var _best in best_wor)
+            {
+                Console.WriteLine($"Лучший работник: {_best.Name} {_best.Surname}, {_best.Profession}");
+            }
+
+
+            var best_man = from _manager in managers
+                           where _manager.Useful == BestOfManagers(managers)
+                           select _manager;
+
+            foreach (var _best in best_man)
+            {
+                Console.WriteLine($"Лучший манагер: {_best.Name} {_best.Surname}, {_best.Profession}");
+            }
+        }
+
+
+        static decimal BestOfWorkers(List<Worker> list)
+        {
+            int i = 0;
+            decimal best;
+            decimal[] useful = new decimal[list.Count];
+
+            var best_worker = from worker in list
+                              select worker.Useful;
+
+            foreach (decimal num in best_worker)
+            {
+                useful[i] = num;
+            }
+
+            best = useful.Max();
+
+            return best;
+        }
+
+
+        static decimal BestOfManagers(List<Manager> list)
+        {
+            int i = 0;
+            decimal best;
+            decimal[] useful = new decimal[list.Count];
+
+            var best_manager = from worker in list
+                              select worker.Useful;
+
+            foreach (decimal num in best_manager)
+            {
+                useful[i] = num;
+            }
+
+            best = useful.Max();
+
+            return best;
+        }
+
+
+
+
+
+
+    }
+
+
+}
---
+++ Homework2/Workers.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    
+    class Worker
+    {
+        
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public decimal Payment { get; set; } //зарплата
+        public decimal Useful { get; set; } //выработка
+        public string Profession { get; set; }
+
+        public Worker(string name, string surname, decimal payment, decimal useful, string profession)
+    {
+        Name = name;
+        Surname = surname;
+        Payment = payment;
+        Useful = useful;
+        Profession = profession;
+    }
+    
+        public static List<Worker> GetSampleWorkers()
+        {
+            return new List<Worker>
+            {
+                new Worker(name : "Иван", surname : "Иванов", payment : 45000,
+            useful : 155000, profession : "плотник"),
+                new Worker(name : "Петр", surname : "Петров", payment : 55000,
+            useful : 156000, profession : "бетонщик"),
+                new Worker(name : "Сидор", surname : "Сидоров", payment : 50000,
+            useful : 163000, profession : "сварщик"),
+            };
+        }
+
+
+    }
+
+
+    class Manager
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public decimal Payment { get; set; } //зарплата
+        public decimal Useful { get; set; } //выработка
+        public string Profession { get; set; }
+
+        public Manager(string name, string surname, decimal payment, decimal useful, string profession)
+        {
+            Name = name;
+            Surname = surname;
+            Payment = payment;
+            Useful = useful;
+            Profession = profession;
+        }
+
+        public static List<Manager> GetSampleManagers()
+        {
+            return new List<Manager>
+            {
+                new Manager(name : "Сергей", surname : "Сергеев", payment : 35000,
+            useful : 140000, profession : "менеджер по продажам"),
+                new Manager(name : "Михаил", surname : "Михайлов", payment : 42000,
+            useful : 158000, profession : "менеджер по рекламе"),
+                new Manager(name : "Кирилл", surname : "Кириллов", payment : 65000,
+            useful : 166000, profession : "менеджер по еще чему-то"),
+            };
+        }
+    }
+
+}
+
